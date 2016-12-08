3cdeb4c9b5696ca00771ae7c01f6b3cc030e472f
quzhe <supermp@foxmail.com>
2016-12-08 19:55:48 +0800

    Use proper path in entrypoint.sh

--- asf/entrypoint.sh
+++ asf/entrypoint.sh
@@ -14,4 +14,4 @@ done
 sed -i 's/DefaultUsername/'"$USERNAME"'/' "$DIR"/config/default.json
 sed -i 's/DefaultPassword/'"$PASSWORD"'/' "$DIR"/config/default.json
 
-mono ASF.exe
+mono "$DIR"/ASF.exe
